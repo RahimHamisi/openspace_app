3ec40280b43a5c0525c9f0c1c2e63f2aa5861b3c
RahimHamisi <rahimramadhani2502@gmail.com>
2025-03-13 06:21:43 +0300

    Delete open_space_application directory with replacing the new ones due to some misconfiguration in dependency

--- open_space_application/android/app/src/main/kotlin/com/example/open_space_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.open_space_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- open_space_application/lib/utils/map_helper.dart
+++
@@ -1,8 +0,0 @@
-import 'package:latlong2/latlong.dart';
-
-// Function to handle map tap and return tapped position
-LatLng onMapTap(LatLng tapPosition) {
-  // You can add custom logic here to handle the tap (e.g., show the coordinates, trigger an action)
-  print("Tapped position: ${tapPosition.latitude}, ${tapPosition.longitude}");
-  return tapPosition;
-}
